35c32bb1f43e6d26bb509695e5234a7bf6277dde
Nikolay Stebunov <ponochevnii@mail.ru>
2023-03-12 17:26:51 +0300

    add layout styles

--- gulpfile.js
+++ gulpfile.js
@@ -129,7 +129,7 @@ const reload = (done) => {
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
   gulp.watch('source/js/**/*.js', gulp.series(scripts));
-  gulp.watch('source/*.html'), gulp.series(html, reload);
+  gulp.watch('source/*.html', gulp.series(html, reload));
 }
 
 
